3b4fa794e6661dbcd82254f34baec846ac8c880b
Strasho Naumov <47228058+naumovvvs@users.noreply.github.com>
2021-02-12 19:07:38 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/macvilla/MacvillaApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.macvilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacvillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacvillaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/wp/macvilla/MacvillaApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.macvilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacvillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
