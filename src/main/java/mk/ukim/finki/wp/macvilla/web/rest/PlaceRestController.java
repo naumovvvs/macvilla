819232d370b842ffce72a05be4e0c37407c4881a
Strasho Naumov <47228058+naumovvvs@users.noreply.github.com>
2021-02-25 23:28:52 +0100

    Add Place REST Controller and modify links in navbar

---
+++ src/main/java/mk/ukim/finki/wp/macvilla/web/rest/PlaceRestController.java
@@ -0,0 +1,25 @@
+package mk.ukim.finki.wp.macvilla.web.rest;
+
+import mk.ukim.finki.wp.macvilla.model.Place;
+import mk.ukim.finki.wp.macvilla.model.exceptions.PlaceNotFoundException;
+import mk.ukim.finki.wp.macvilla.service.PlaceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/place")
+public class PlaceRestController {
+
+    private final PlaceService placeService;
+
+    public PlaceRestController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/{id}")
+    public Place getPlace(@PathVariable Long id){
+        return this.placeService.findById(id).orElseThrow(() -> new PlaceNotFoundException(id));
+    }
+}
